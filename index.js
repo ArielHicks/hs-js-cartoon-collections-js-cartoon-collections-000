6dbdc3fe70752e0ac7641a631f011fc2ac4f9e91
Ariel Hicks <hicks.ariel@gmail.com>
2018-10-23 02:14:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function dwarfRollCall(dwarves) {
     console.log("1 " + dwarf[0] + ", 2 " + dwarf[1] + ", 3 " + dwarf[2] + ", 4 " + dwarf[3] + ", 5 " + dwarf[4] + ", 6 " + dwarf[5] + ", 7 " + dwarf[6])
-    
+
 }
 
 function summonCaptainPlanet(planeteerCalls){
